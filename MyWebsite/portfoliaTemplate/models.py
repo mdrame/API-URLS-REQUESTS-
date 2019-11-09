66533112059cde3d536965df14069fc642ce8bad
mohammed <mdrame113@gmail.com>
2019-11-09 14:30:19 -0800

    touch requirements.txt and initiate Django models

--- MyWebsite/portfoliaTemplate/models.py
+++ MyWebsite/portfoliaTemplate/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+# class projectImage(models.Model):
+
+# class projectLink(models.Model):
+
+# project name class object
+class ProjectName(models.Model):
+    name_text = models.CharField(max_length=200)
+
+
+
+#project description class object 
+class ProjectDescription(models.Model):
+    descrition_text = models.CharField(mix_length=1000)
+
+
+# class ProjectName(models.Model):
+
+# class projectLink(models.Model):
+
+
+    
