93ec16ab3c3eb160f520f76d8bbbe0d0be7ac526
mohammed <mdrame113@gmail.com>
2019-11-13 13:30:09 -0800

    project running

--- MyWebsite/MyWebsite/settings.py
+++ MyWebsite/MyWebsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfile s',
+    'django.contrib.staticfiles',
     'portfoliaTemplate',
 ]
 
