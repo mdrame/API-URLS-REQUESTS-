1a97e7bac4f2edb9aef9850eb9273759f48e1ec3
mohammed <mdrame113@gmail.com>
2019-12-10 16:06:33 -0800

    PotfolioProject

--- Project/portfolioSite/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Model, PortfolioModel
-
-# Register your models here.
-
-admin.site.register(Model)  
-admin.site.register(PortfolioModel) 
--- Project/portfolioSite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfoliositeConfig(AppConfig):
-    name = 'portfolioSite'
